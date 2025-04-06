5d8c3b0c981016f461ef7c9badf3b6a5d0b85af6
abdul-razith <abdulrazith10113a@gmail.com>
2025-04-06 11:12:49 +0530

    Update FitLife360 image and project description

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { motion } from "framer-motion"
 import { FaHtml5, FaCss3Alt, FaReact, FaGithub, FaGlobe } from "react-icons/fa6";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiVercel } from "react-icons/si";
 import { SiJavascript } from "react-icons/si";
 import { RiFirebaseFill } from "react-icons/ri";
+import { SiNextdotjs } from "react-icons/si";
 import './projects.css'
 
 export const Projects = () => {
@@ -17,12 +20,66 @@ export const Projects = () => {
             <h2 className='font-fontHead text-3xl mb-10 text-center'>Projects</h2>
 
             <motion.div
+                variants={fadeInUp}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.5 }}
+                className='row bg-colorBg border-2 border-colorText rounded-2xl p-10 flex flex-col gap-y-3 xl:flex-row-reverse lg:gap-y-5 justify-between items-center mb-8'>
+                <div className='2xl:w-[60%] xl:w-[50%] w-full hidden sm:block'>
+                    <img src="fitlife360.png" alt="" className='rounded-lg' />
+                </div>
+                <div className='2xl:w-[36%] xl:w-[45%] w-full text-base font-medium flex flex-col gap-y-5 xl:justify-between text-justify font-fontBody leading-relaxed'>
+                    <h3 className='text-xl font-semibold'>FitLife360</h3>
+                    <p>&emsp;&emsp;A dynamic and responsive blog application built using HTML, Tailwind CSS, Next.js, and MongoDB. Features include a hero slider, category filters, and search functionality, with top scores in performance, accessibility, and SEO.</p>
+                    <div className='tech flex gap-y-2 flex-wrap justify-evenly'>
+                        <div>
+                            <FaHtml5 />
+                            <p>HTML</p>
+                        </div>
+                        <div>
+                            <RiTailwindCssFill />
+                            <p>Tailwind</p>
+                        </div>
+                        <div>
+                            <SiJavascript />
+                            <p>JS</p>
+                        </div>
+                        <div>
+                            <SiNextdotjs />
+                            <p>Next JS</p>
+                        </div>
+                        <div>
+                            <SiVercel />
+                            <p>Vercel</p>
+                        </div>
+                    </div>
+                    <div className='btns flex justify-between xl:justify-between duration-500'>
+                        <motion.a href="https://github.com/abdul-razith/blog-app" target='_blank' whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+                            <button>
+                                <FaGithub />
+                                <p>GitHub</p>
+                            </button>
+                        </motion.a>
+                        <motion.a href="https://www.fitlife360.life/" target='_blank' whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+                            <button>
+                                <FaGlobe />
+                                <p>Live</p>
+                            </button>
+                        </motion.a>
+                    </div>
+                </div>
+            </motion.div>
+
+            {/* ------------- */}
+
+            <motion.div
 
                 variants={fadeInUp}
                 initial="hidden"
                 whileInView="visible"
-                viewport={{once:true, amount: 0.2 }}
-                transition={{ duration: 0.5}}
+                viewport={{ once: true, amount: 0.2 }}
+                transition={{ duration: 0.5 }}
 
                 className='row bg-colorBg border-2 border-colorText rounded-2xl p-10 flex flex-col gap-y-3 xl:flex-row lg:gap-y-5 justify-between items-center mb-8'>
                 <div className='2xl:w-[60%] xl:w-[50%] w-full hidden sm:block'>
@@ -73,12 +130,12 @@ export const Projects = () => {
             {/* ----- */}
 
             <motion.div
-            variants={fadeInUp}
-            initial="hidden"
-            whileInView="visible"
-            viewport={{once:true, amount: 0.5 }}
-            transition={{ duration: 0.5}}
-            className='row bg-colorBg border-2 border-colorText rounded-2xl p-10 flex flex-col gap-y-3 xl:flex-row-reverse lg:gap-y-5 justify-between items-center mb-8'>
+                variants={fadeInUp}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.5 }}
+                className='row bg-colorBg border-2 border-colorText rounded-2xl p-10 flex flex-col gap-y-3 xl:flex-row-reverse lg:gap-y-5 justify-between items-center mb-8'>
                 <div className='2xl:w-[60%] xl:w-[50%] w-full hidden sm:block'>
                     <img src="greenplate.png" alt="" className='rounded-lg' />
                 </div>
